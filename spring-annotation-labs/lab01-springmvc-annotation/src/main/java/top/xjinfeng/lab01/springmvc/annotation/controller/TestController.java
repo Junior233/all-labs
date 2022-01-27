92f21de2b214c79188d099e361415ec08768f618
Junior233 <67447332+Junior233@users.noreply.github.com>
2022-01-27 17:33:15 +0800

    增加测试方法

--- spring-annotation-labs/lab01-springmvc-annotation/src/main/java/top/xjinfeng/lab01/springmvc/annotation/controller/TestController.java
+++ spring-annotation-labs/lab01-springmvc-annotation/src/main/java/top/xjinfeng/lab01/springmvc/annotation/controller/TestController.java
@@ -1,8 +1,6 @@
 package top.xjinfeng.lab01.springmvc.annotation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.xjinfeng.lab01.springmvc.annotation.model.Car;
 import top.xjinfeng.lab01.springmvc.annotation.model.User;
 
@@ -10,8 +8,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 描述:
- *
+ * 描述: @RequestParam和不带注解 主要能处理k-v类型的请求query参数（如 /test?name=gg）, get请求一般不带body参数, 因为很多http服务端都会忽略它
+ *       get请求是支持传body参数的 {@link TestController#getRequestParam(String)}
+ *       @RequestBody 用于处理json类型的请求参数 多用于post请求或其衍生请求（如put delete patch ） 一般作为body参数传递
+ * 下面测试采用的请求方式跟java方法能处理的请求方式一一对应
  * @author xu.jin.feng
  * @create 2022/1/27 14:57
  */
@@ -44,17 +44,17 @@ public class TestController {
     /**
      * RequestParam注解
      *
-     * http://localhost:8080/testRequestParam?user.name=45&user.age=33&car.name=45&car.cost=33
+     * http://localhost:8080/testRequestParamMore?user.name=45&user.age=33&car.name=45&car.cost=33
      * Request URL: http://localhost:8080/testRequestParam
      * Status Code: 400
      * There was an unexpected error (type=Bad Request, status=400).
      * Required request parameter 'user' for method parameter type User is not present
      *
-     * http://localhost:8080/testRequestParam?user={name:45,age=33}&car={name:45,cost=33}
+     * http://localhost:8080/testRequestParamMore?user={name:45,age=33}&car={name:45,cost=33}
      * java.lang.IllegalArgumentException: Invalid character found in the request target
      * [/testRequestParam?user={name:45,age=33}&car={name:45,cost=33} ]. The valid characters are defined in RFC 7230 and RFC 3986
      *
-     * http://localhost:8080/testRequestParam?user=
+     * http://localhost:8080/testRequestParamMore?user=
      * Status Code: 500
      * nested exception is java.lang.IllegalStateException: Cannot convert value of type 'java.lang.String' to required type
      *
@@ -82,7 +82,7 @@ public class TestController {
 
     /**
      * 无法接收处理key 为 user, value为 User类型的参数对象
-     * http://localhost:8080/testRequestParam?name=1
+     * http://localhost:8080/testRequestParamCustom?name=1
      * There was an unexpected error (type=Bad Request, status=400).
      * Required request parameter 'user' for method parameter type User is not present
      *
@@ -106,6 +106,68 @@ public class TestController {
     public Map<String, Object> testRequestParamMix(@RequestParam String name, User user){
         Map<String, Object> map =  new HashMap<>(2);
         map.put("user", user);
+        map.put("name", name);
+        return map;
+    }
+
+    /**
+     * http://localhost:8080/postRequestParam
+     * body参数 name:bb
+     * content-type 为 multipart/form-data 或 application/x-www-form-urlencoded
+     * {
+     * 	"name": "bb"
+     * }
+     *
+     * 或
+     * query参数 http://localhost:8080/postRequestParam?name=bb
+     * {
+     * 	"name": "bb"
+     * }
+     *
+     * body参数和query参数 同时设置
+     * {
+     * 	"name": "bb,bb"
+     * }
+     *
+     */
+    @PostMapping("/postRequestParam")
+    public Map<String, Object> postRequestParam(@RequestParam String name){
+        Map<String, Object> map =  new HashMap<>(2);
+        map.put("name", name);
+        return map;
+    }
+
+    /**
+     * http://localhost:8080/postRequestParam
+     * body参数 name:bb
+     * content-type 为 multipart/form-data
+     * {
+     * 	"name": "bb"
+     * }
+     * 为application/x-www-form-urlencoded
+     * 	"status": 400,
+     * 	"error": "Bad Request",Required request parameter 'name' for method parameter type String is not present
+     *
+     *
+     * query参数 http://localhost:8080/postRequestParam?name=bb
+     * {
+     * 	"name": "bb"
+     * }
+     *
+     * body参数和query参数 同时设置
+     * 当为 multipart/form-data
+     * {
+     * 	"name": "bb,bb"
+     * }
+     *
+     * 当为application/x-www-form-urlencoded
+     * {
+     * 	"name": "bb"
+     * }
+     */
+    @GetMapping("/getRequestParam")
+    public Map<String, Object> getRequestParam(@RequestParam String name){
+        Map<String, Object> map =  new HashMap<>(2);
         map.put("name", name);
         return map;
     }
